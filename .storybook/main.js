2b1f38de847b7302c91732368bc191e4c64471f5
harry(정호일) <harry.hi@kakaocorp.com>
2020-01-20 18:56:51 +0900

    스토리북 typescript 설정

---
+++ .storybook/main.js
@@ -0,0 +1,23 @@
+module.exports = {
+  stories: ['../stories/**/*.stories.tsx'],
+  addons: [
+    '@storybook/preset-typescript',
+    '@storybook/addon-actions',
+    '@storybook/addon-links'
+  ],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('ts-loader')
+        },
+        {
+          loader: require.resolve('react-docgen-typescript-loader')
+        }
+      ]
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  }
+};
